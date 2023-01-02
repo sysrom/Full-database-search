f8b1608b2891e3ea210bc6337a1c475510fd257c
sysrom <88297660+sysrom@users.noreply.github.com>
2023-01-02 21:59:33 +0800

    Add files via upload

---
+++ Database_Search.py
@@ -0,0 +1,33 @@
+import pymysql
+
+keyword='input you want to search the keyword'
+cnx = pymysql.connect(user='Database_Username', password='Database_Password', host='Database_host',port=3306,database='database_name')
+
+cursor = cnx.cursor()
+
+cursor.execute("SHOW TABLES")
+tables = cursor.fetchall()
+
+for table in tables:
+    table_name = table[0]
+    
+    cursor.execute("DESCRIBE " + table_name)
+    fields = cursor.fetchall()
+    
+    select_stmt = "SELECT "
+    for field in fields:
+        select_stmt += field[0] + ", "
+    select_stmt = select_stmt[:-2] + " FROM " + table_name
+    
+    query = select_stmt + " WHERE "
+    for field in fields:
+        query += field[0] + " LIKE '"+keyword+"' OR "
+    query = query[:-4]
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+cursor.close()
+cnx.close()
